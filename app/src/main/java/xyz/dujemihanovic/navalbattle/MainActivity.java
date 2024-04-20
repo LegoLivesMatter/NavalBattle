2675ad41905e96bfcd1166cd3fae05cee6676cf5
Duje Mihanović <duje.mihanovic@skole.hr>
2024-04-20 17:09:48 +0200

    Initial commit

---
+++ app/src/main/java/xyz/dujemihanovic/navalbattle/MainActivity.java
@@ -0,0 +1,36 @@
+package xyz.dujemihanovic.navalbattle;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button exit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        exit = findViewById(R.id.btnExit);
+        exit.setOnClickListener(v -> {
+            super.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_USER);
+            finish();
+        });
+    }
+}
