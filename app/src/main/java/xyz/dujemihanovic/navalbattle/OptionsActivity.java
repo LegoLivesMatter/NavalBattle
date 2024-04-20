ef3300f436401478be98b18ec2051b1b5f2d4060
Duje Mihanović <duje.mihanovic@skole.hr>
2024-04-20 19:50:20 +0200

    Add placeholder options page

--- app/src/main/java/xyz/dujemihanovic/navalbattle/OptionsActivity.java
+++ app/src/main/java/xyz/dujemihanovic/navalbattle/OptionsActivity.java
@@ -1,9 +1,7 @@
 package xyz.dujemihanovic.navalbattle;
 
-import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,33 +9,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
-    Button exit, options;
-
+public class OptionsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        setContentView(R.layout.activity_options);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        exit = findViewById(R.id.btnExit);
-        exit.setOnClickListener(v -> {
-            super.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_USER);
-            finish();
-        });
-
-        options = findViewById(R.id.btnOptions);
-        options.setOnClickListener(v -> {
-            Intent intent = new Intent();
-            intent.setClass(this, OptionsActivity.class);
-            startActivity(intent);
         });
     }
 }
